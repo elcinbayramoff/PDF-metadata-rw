a18ed1b6e06e7fe85fd0d90d34930e73513b2276
elcinbayramoff <bayramlielcin03@gmail.com>
2024-03-24 13:44:57 +0400

    Fixed the bug
    Made some changes to the PyPDF2 source code.

--- Readme.md
+++ Readme.md
@@ -2,3 +2,20 @@
 
 
 Once, when I edited and downloaded my CV from Canva, I encountered an unexpected issue: the title of the PDF file remained the same, making it seem unprofessional. Despite my attempts to edit it locally, I couldn't find a straightforward solution. Frustrated by this inconvenience, I sought a workaround and eventually turned to online platforms for editing metadata. However, this experience inspired me to take matters into my own hands and develop a solution tailored to my needs. Leveraging the power of ```tkinter``` and ```PyPDF2```, I embarked on creating a tool to efficiently manage PDF metadata. This project not only addressed my initial problem but also empowered me to proactively solve similar challenges in the future.
+
+## Important!!!
+Overwrite the ``` _writer.py ``` file inside PyPDF2 library and add replace the code at 156-165th lines with
+```
+        info = DictionaryObject()
+        info.update(
+            {
+                NameObject("/Producer"): create_string_object(
+                    codecs.BOM_UTF16_BE + "".encode("utf-16be")
+                )
+            }
+        )
+        self._info = self._add_object(info)
+```
+While deleting all the metadata, it stills assign 'PyPDF2' value to the Producer key. That's why we need to change the source code.
+
+Or you can just replace ``` _writer.py ``` file with the file provided in the repository.
--- changer.py
+++ changer.py
@@ -8,6 +8,9 @@ def MetaReader(path_to_pdf):
 def MetaWriter(Creator,Producer,CreationDate,ModDate,KeyWords,Author,Title,path_to_pdf):
     writer = PdfWriter()
     reader = PdfReader(path_to_pdf)
+    
+    for page in reader.pages:
+        writer.add_page(page)
     metadata = {
         '/Creator': Creator,
         '/Producer': Producer,
@@ -18,13 +21,13 @@ def MetaWriter(Creator,Producer,CreationDate,ModDate,KeyWords,Author,Title,path_
     
     if CreationDate:
         metadata['/CreationDate'] = back_formatter(CreationDate)
-    
+    else:
+        metadata['/CreationDate'] = CreationDate
     if ModDate:
         metadata['/ModDate'] = back_formatter(ModDate)
-
-    for page in reader.pages:
-        writer.add_page(page)
-
+    else:
+        metadata['/ModDate'] = ModDate
+    writer.add_metadata(metadata)
     with open(path_to_pdf, "wb") as f:
         writer.write(f)
 def PdfMeta(path_to_pdf):
@@ -35,18 +38,20 @@ def PdfMeta(path_to_pdf):
         else: state= False
     if state:
         meta = MetaReader(path_to_pdf)
-        try:
-            Creator, Producer, CreationDate, ModDate, Keywords, Author, Title = \
-                meta['/Creator'], meta['/Producer'], meta['/CreationDate'], meta['/ModDate'], meta['/Keywords'], meta['/Author'], meta['/Title']
-            Cdate = formatter(CreationDate)
+        # try:
+        Creator, Producer, CreationDate, ModDate, Keywords, Author, Title = \
+            meta['/Creator'], meta['/Producer'], meta['/CreationDate'], meta['/ModDate'], meta['/Keywords'], meta['/Author'], meta['/Title']
+        Cdate = formatter(CreationDate)
+        if CreationDate:
             CreationDate = toLocalUTC(int(Cdate['year']), int(Cdate['month']), int(Cdate['day']), \
                 int(Cdate['hour']), int(Cdate['minute']), int(Cdate['second']))
             Cdate = formatter(ModDate)
+        if ModDate:
             ModDate = toLocalUTC(int(Cdate['year']), int(Cdate['month']), int(Cdate['day']), \
                 int(Cdate['hour']), int(Cdate['minute']), int(Cdate['second']))
-        
-        except:
-            Creator, Producer, CreationDate, ModDate, Keywords, Author, Title='','','','','','',''
+    
+        # except:
+        #     Creator, Producer, CreationDate, ModDate, Keywords, Author, Title='','','','','','',''
         return [Creator, Producer, CreationDate, ModDate, Keywords, Author, Title]
     else:
         return False
